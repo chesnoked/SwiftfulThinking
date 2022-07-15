c8c27d37553098b660983a2cf33b42b20c779530
chesnoked <dreamspiritworld@gmail.com>
2022-07-15 12:13:07 +1000

    SwiftfulThinking #3. '08. How to use mask. Create a 5-star rating button'

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -0,0 +1,60 @@
+//
+//  MaskBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Evgeniy Safin on 15.07.2022.
+//
+
+import SwiftUI
+
+struct MaskBootcamp: View {
+    
+    @State var rating: Int = 3
+    
+    var body: some View {
+        ZStack {
+            Color.black
+            starsView
+                .overlay(
+                    overlayView
+                        .mask(starsView)
+                )
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    //.foregroundColor(.yellow)
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: [Color.purple, Color.pink]),
+                        startPoint: .leading,
+                        endPoint: .trailing))
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.gray)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct MaskBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MaskBootcamp()
+    }
+}

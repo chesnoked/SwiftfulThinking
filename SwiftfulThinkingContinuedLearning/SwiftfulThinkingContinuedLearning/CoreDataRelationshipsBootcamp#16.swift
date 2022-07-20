100d2755bfc730ee32900c53024f00fde02ff8b9
chesnoked <dreamspiritworld@gmail.com>
2022-07-20 15:42:01 +1000

    SwiftfulThinking #3. '15. How to use CoreData with MVVM.'

---
+++ SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/CoreDataRelationshipsBootcamp#16.swift
@@ -0,0 +1,22 @@
+//
+//  CoreDataRelationshipsBootcamp#16.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Evgeniy Safin on 21.07.2022.
+//
+
+import SwiftUI
+
+
+
+struct CoreDataRelationshipsBootcamp_16: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoreDataRelationshipsBootcamp_16_Previews: PreviewProvider {
+    static var previews: some View {
+        CoreDataRelationshipsBootcamp_16()
+    }
+}

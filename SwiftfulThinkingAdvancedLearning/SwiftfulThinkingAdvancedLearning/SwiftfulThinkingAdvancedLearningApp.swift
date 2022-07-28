c5e524c534af72ddc400a4451f1753260a04ea40
chesnoked <dreamspiritworld@gmail.com>
2022-07-28 10:59:37 +1000

    SwiftfulThinking #3. '23. Download JSON posts with Combine framework.'

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Evgeniy Safin on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppNavBarView()
+        }
+    }
+}
